277a7fbd3407b804f31ef058d7c27d80eaed7ce2
Dharmik Vaghela <dharmikvaghela8869@gmail.com>
2025-12-02 11:14:42 +0530

    add

--- MVC/server.js
+++ MVC/server.js
@@ -2,8 +2,11 @@ const express = require("express")
 const db = require("./config/db")
 const U_router = require("./Routes/userRouter")
 const app = express()
+const user = require("./model/usermodel")
+const B_router = require("./Routes/bookRoutes")
 
 app.use(express.json())
+app.use('/Book',B_router)
 
 app.use("/user",U_router)
 
